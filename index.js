ac76cf46783ff93b7a6833899b60fd3486215c4a
vkavith <vkavith@gmail.com>
2025-01-11 17:58:14 -0500

    SBA 318 Express Server Application

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 3000;
 
 //const areas = require("./data/area");
-
+//Import Routes
 const areaRoutes = require("./routes/areaRoutes");
 const mealRoutes = require("./routes/mealRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 
 const bodyParser = require("body-parser");
+
+//Import middleware
+const requestLogger = require("./middleware/requestLogger");
+const errorHandler = require("./middleware/errorHandler");
 
 //Routes
 
@@ -17,18 +21,23 @@ app.use("/api/meals", mealRoutes);
 app.use("/api/category", categoryRoutes);
 //Middleware
 app.use(express.json());
+app.use(requestLogger);
 
+//Home Route
 app.get("/", (req, res) => {
   res.send("Welcome to MealDB API");
 });
 
-//end of route if areaID doesnt exist
+//404 Handler
 
 app.use((req, res) => {
   res.status(404);
   res.json({ error: "resource not found" });
 });
 
+//Error Handler
+app.use(errorHandler);
+
 app.listen(port, () => {
-  console.log(`server listening on: ${port}`);
+  console.log(`Server listening on port: ${port}`);
 });
---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json( {
+        error: 'Something went wrong',
+        message: err.message
+    });
+};
+
+module.exports = errorHandler;
